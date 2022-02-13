9a111130e459f033fd4d91278eedcc58bbfd1b39
hoschmidt2020 <82689238+hoschmidt2020@users.noreply.github.com>
2022-02-13 14:41:40 -0600

    commit1

--- player.sql
+++ player.sql
@@ -1,11 +1,6 @@
 
 --Create table
-CREATE TABLE player (
-  id INT,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  codename VARCHAR(30)
-);
+CREATE TABLE player (id INT, first_name VARCHAR(30),last_name VARCHAR(30),codename VARCHAR(30));
 
 --Place first record into table
 INSERT INTO player (id, first_name, last_name, codename)
